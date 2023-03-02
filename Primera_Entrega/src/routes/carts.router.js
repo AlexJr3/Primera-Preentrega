4f972d246d1b2642b84295e991f75d4784272cc8
alex_rb <alexisjrb2908@gmail.com>
2023-03-02 11:27:32 -0300

    test cartRouter

--- Primera_Entrega/src/routes/carts.router.js
+++ Primera_Entrega/src/routes/carts.router.js
@@ -8,4 +8,14 @@ const cartsRouter = Router();
 //MIDDLEWARES
 cartsRouter.use(json());
 
-cartsRouter;
+cartsRouter.get("/:cid", async (req, res) => {
+  try {
+    const cid = Number(req.params.cid);
+    const carts = await cartManager.getCartById(cid);
+    res.status(200).send(carts);
+  } catch (error) {
+    res.status(404).send({ error });
+  }
+});
+
+cartsRouter
